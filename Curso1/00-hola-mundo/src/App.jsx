5da4553c64aefc4ea82bb62233dce93685f68536
CarlosAlvarado-git <61555440+CarlosAlvarado-git@users.noreply.github.com>
2024-08-16 18:59:07 -0600

    feat(Curso1): :sparkles: First component with react
    This is just the component, but it is not reusable

--- Curso1/00-hola-mundo/src/App.jsx
+++ Curso1/00-hola-mundo/src/App.jsx
@@ -1,7 +1,22 @@
+import './App.css'
 export function App () {
     return (
-        <div>
-            <h1>Twitter Card</h1>
-        </div>
+        <article className='tw-followCard'>
+            <header className='tw-followCard-header'>
+                <img  
+                    className='tw-followCard-avatarimg'
+                    alt="El avatar de Midudev" src="https://unavatar.io/midudev" />
+                <div className='tw-followCard-info'>
+                    <strong>Miguel Angel Duran</strong>
+                    <span className='tw-followCard-info-userName'>@midudev</span>
+                </div>
+            </header>
+
+            <aside>
+                <button className='tw-followCard-button'>
+                    Seguir
+                </button>
+            </aside>
+        </article>
     )
 }
